b350e0be375409ce19b13f77c0fd4435427d4a36
mjohanbintangp <m.johanbintang.pratama@gmail.com>
2026-01-12 11:49:57 +0700

    add login UI - shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "bwa-mjohanbintangp",
+  description: "build with angga course - leaarning purpose - mjohanbintangp",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
